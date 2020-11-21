a1f293bacdde966889f713c506b91e104b46dbcd
12323-ang <74833260+12323-ang@users.noreply.github.com>
2020-11-22 00:26:39 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,33 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { WeatherComponent } from './weather/weather.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import {FormsModule} from '@angular/forms';
+import { WeatherData } from './weather-data';
+
+import { WeatherService } from './services/weather.service';
+import { environment } from '../environments/environment';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+   declarations: [
+      AppComponent,
+      WeatherComponent
+
+   ],
+   imports: [
+BrowserModule,
+      AppRoutingModule,
+      HttpClientModule,
+      AngularFontAwesomeModule,
+      FormsModule,
+   ],
+   providers: [WeatherService],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
